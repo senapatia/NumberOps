fcb709f945dce7bb3355d1f9c4d57f25f8495477
senapatia <senapatia322@gmail.com>
2021-07-23 09:38:35 +0530

    first commit

---
+++ src/main/java/operation/number/app/AddNumber.java
@@ -0,0 +1,26 @@
+package operation.number.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddNumber extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int firstNumber = Integer.parseInt(req.getParameter("num1"));
+		int secondNumber = Integer.parseInt(req.getParameter("num2"));
+		int sum = firstNumber + secondNumber;
+// PrintWriter out = res.getWriter();
+// res.getWriter().println("result is = "+k);
+//		req.setAttribute("K", k);
+//		RequestDispatcher rd = req.getRequestDispatcher("Square");
+//		rd.forward(req, res);
+
+		res.sendRedirect("Square?sum=" + sum);
+	}
+}
---
+++ src/main/java/operation/number/app/SquareNumber.java
@@ -0,0 +1,20 @@
+package operation.number.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SquareNumber extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		int sum = Integer.parseInt(req.getParameter("sum"));
+		int Result = sum * sum;
+		PrintWriter out = res.getWriter();
+		out.println("Square Of Sum Of The Two Numbers Is " + Result);
+		System.out.println("Square called");
+	}
+}
