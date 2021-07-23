ff843fc0dc6a523e699890bde45e0efa28f12cfa
senapatia <senapatia322@gmail.com>
2021-07-23 09:45:25 +0530

    Minor Formatting V2

--- src/main/java/operation/number/app/SquareNumber.java
+++ src/main/java/operation/number/app/SquareNumber.java
@@ -12,9 +12,12 @@ public class SquareNumber extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
 		int sum = Integer.parseInt(req.getParameter("sum"));
+		
 		int Result = sum * sum;
+		
 		PrintWriter out = res.getWriter();
 		out.println("Square Of Sum Of The Two Numbers Is " + Result);
-		System.out.println("Square called");
+		// System.out.println("Square called");
+
 	}
 }
